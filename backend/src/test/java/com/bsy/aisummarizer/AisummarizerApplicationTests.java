a89c28d4223ff1471caeeb74497cbb956f32d456
BSY3510 <baeksy3510@gmail.com>
2026-03-27 01:22:52 +0900

    feat: Spring Boot 백엔드 초기 세팅 및 properties 구성

---
+++ backend/src/test/java/com/bsy/aisummarizer/AisummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsy.aisummarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AisummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
